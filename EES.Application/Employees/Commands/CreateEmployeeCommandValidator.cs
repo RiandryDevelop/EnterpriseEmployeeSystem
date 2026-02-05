4d653f1f56915c0e66e5d309bf76df172a890c70
RiandryDevelop <riandrydevs@gmail.com>
2026-02-04 20:21:10 -0400

    feat(app): add fluent validation for employee create and update commands

---
+++ EES.Application/Employees/Commands/CreateEmployeeCommandValidator.cs
@@ -0,0 +1,26 @@
+﻿using FluentValidation;
+
+namespace EES.Application.Employees.Commands;
+
+public class CreateEmployeeCommandValidator : AbstractValidator<CreateEmployeeCommand>
+{
+    public CreateEmployeeCommandValidator()
+    {
+        RuleFor(x => x.FirstName)
+            .NotEmpty().MaximumLength(50);
+
+        RuleFor(x => x.LastName)
+            .NotEmpty().MaximumLength(50);
+
+        RuleFor(x => x.Email)
+            .NotEmpty().EmailAddress()
+            .WithMessage("A valid corporate email address is required.");
+
+        RuleFor(x => x.JobTitle)
+            .NotEmpty().MaximumLength(100);
+
+        RuleFor(x => x.HireDate)
+            .NotEmpty().LessThanOrEqualTo(DateTime.Now)
+            .WithMessage("Hire date cannot be in the future.");
+    }
+}
